f4ab84dd60be8b8d4b565fa086fab7ba9ad15698
kamran1122 <kamrannazir692@gmail.com>
2018-03-20 01:56:20 -0700

    added link to the emails page

--- src/Faq.js
+++ src/Faq.js
@@ -23,6 +23,7 @@ class Faq extends Component {
 							<img src="./JAMES App Landing Page_files/logo_light.png" alt="logo" className="logo"/>
 							<nav className="navigation_desktop">
 								<ul>
+									<li className=""><a href="javascript:void(0)" onClick={()=> this.props.history.push('/emails')} className="visible-lg visible-md">Emails</a></li>
 									<li className=""><a href="javascript:void(0)" className="visible-lg visible-md go_to_overview" onClick={()=> this.props.history.push('/faq')} >FAQ</a></li>
 									<li className=""><a href="javascript:void(0)" onClick={()=> this.props.history.push('/about')} className="visible-lg visible-md">About</a></li>
 									<li className=""><a href="javascript:void(0)" onClick={()=> this.props.history.push('/customers')} className="visible-lg visible-md">Customers</a></li>
